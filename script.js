fee0de83b55d2ead0afad127fe12f9fb9912ff4a
Cyril <c.mougin@hotmail.fr>
2022-03-25 22:27:52 +0100

    add buttons interactio and add a reset button

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ function playRound(playerSelection, computerSelection){
     
     let playerScore = document.querySelector("#player-score");
     let computerScore = document.querySelector("#computer-score");
+    let roundResult = document.querySelector('#round-result');
 
     if ( playerSelection == computerSelection) 
     {
         result = 2;
-        console.log("draw");
+        roundResult.textContent = "draw"
+         
     }
     else if (playerSelection == 'rock'    && computerSelection == "scissor"  ||
              playerSelection == 'scissor' && computerSelection == "paper"    ||
@@ -25,41 +27,78 @@ function playRound(playerSelection, computerSelection){
             
             newScore = parseInt(playerScore.textContent) + 1 ;
             playerScore.textContent = parseFloat(newScore);
+            roundResult.textContent = "player win the round !"
         }
     else {
         newScore = parseInt(computerScore.textContent) + 1 ;
         computerScore.textContent = parseFloat(newScore);
+        roundResult.textContent = "computer win the round !"
     }
 
+    roundResult.style.opacity = "100%";
     MATCHDURATION +=1 ;
 }
-
-
 
 function isGameOver() {
     
     if (MATCHDURATION == 5) {
         const playerButtons = document.querySelectorAll(".btn");
         playerButtons.forEach(button => button.disabled = true);
-        playerButtons.forEach(button => button.style.opacity = 0.5);
+        playerButtons.forEach(button => button.classList.add("noHover"));
         return true;
     }
     return false;
-
 }
 
 
+function winnerDecision() {
 
-function clickEvent(e){
+    let playerScore = parseInt((document.querySelector("#player-score")).textContent);
+    let computerScore = parseInt((document.querySelector("#computer-score")).textContent);
+    let roundResult = document.querySelector('#round-result');
 
+    if (playerScore === computerScore){
+        console.log("it's a draw !")
+        roundResult.textContent = "it's a draw game!"
+    }
+    else if (playerScore > computerScore) {
+        roundResult.textContent = "player wins the game !"
+    }
+    else {
+        roundResult.textContent = "Computer wins the game!"
+    }
+
+
+}
+
+function playerClick(e){
     playRound(e.target.className, computerPlay());
     if(isGameOver()) {
-        console.log("match is over");
+        winnerDecision();
     }
 }
 
+function resetClick(){
+    const playerButtons = document.querySelectorAll(".btn");
+    let playerScore = document.querySelector("#player-score");
+    let computerScore = document.querySelector("#computer-score");
+    let roundResult = document.querySelector('#round-result');
+
+    playerScore.textContent = '0';
+    computerScore.textContent = '0';
+    roundResult.style.opacity = "0%";
+    playerButtons.forEach(button => button.classList.remove("noHover"));
+    playerButtons.forEach(button => button.disabled = false);
+    MATCHDURATION = 0;
+}
+
+
 const playerButtons = document.querySelectorAll(".btn");
-playerButtons.forEach(button => button.addEventListener('click', clickEvent));
+playerButtons.forEach(button => button.addEventListener('click', playerClick));
+
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener('click', resetClick);    
+
     
 
 
