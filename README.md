4462c7f873189cd15b9d8c650918d563ad8df63c
Cyril <c.mougin@hotmail.fr>
2022-03-26 18:07:23 +0100

    add comments and refactor

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# RockPaperScissor-
-javascript game of rock paper scissor
+PROJECT GOAL
+
+The goal of this project is to have a first approach to javascript for web development.
+
+HOW TO USE IT
+
+Open the page on your browser. It's a rock paper scissor game.
+It's a 5 round game.
+Each round you have to push on the button that indicate your play for this round.
+At the end of the 5 rounds, the page shows you who win the game.
+You can also push on the reset button at any time.
+
+STACK
+
+html. css. javascript.
--- script.js
+++ script.js
@@ -1,35 +1,45 @@
+/* allow to count the number of round for the game*/
 let MATCHDURATION = 0;
 
-isGameOver();
+//#region round functions
 
+/**
+* choose randomly between 'rock' 'paper' or 'scissor'
+* @return {string} choosenResultIndex
+*/
 function computerPlay() {
     const result = ["rock", "paper","scissor"];
     let choosenResultindex = result[Math.floor(Math.random() * result.length)];
     return choosenResultindex;
 }
 
-
+/**
+* choose if player or computer wins the round
+* @param {string} playerSelection
+* @param {string} playerSelection
+*/
 function playRound(playerSelection, computerSelection){
     
     let playerScore = document.querySelector("#player-score");
     let computerScore = document.querySelector("#computer-score");
     let roundResult = document.querySelector('#round-result');
 
+    /* loop that decide who win a round */
     if ( playerSelection == computerSelection) 
     {
-        result = 2;
-        roundResult.textContent = "draw"
-         
+        roundResult.textContent = "draw" 
     }
     else if (playerSelection == 'rock'    && computerSelection == "scissor"  ||
              playerSelection == 'scissor' && computerSelection == "paper"    ||
              playerSelection == 'paper'   && computerSelection == "rock") {
             
+            /* get the html text and add +1 to it's vlue*/
             newScore = parseInt(playerScore.textContent) + 1 ;
             playerScore.textContent = parseFloat(newScore);
             roundResult.textContent = "player win the round !"
         }
     else {
+        /* get the html text and add +1 to it's vlue*/
         newScore = parseInt(computerScore.textContent) + 1 ;
         computerScore.textContent = parseFloat(newScore);
         roundResult.textContent = "computer win the round !"
@@ -39,18 +49,29 @@ function playRound(playerSelection, computerSelection){
     MATCHDURATION +=1 ;
 }
 
+
+/**
+* Decide if we went over 5 rounds
+* @return {boolean} true
+* @return {boolean} false
+*/
 function isGameOver() {
     
     if (MATCHDURATION == 5) {
         const playerButtons = document.querySelectorAll(".btn");
-        playerButtons.forEach(button => button.disabled = true);
-        playerButtons.forEach(button => button.classList.add("noHover"));
+        playerButtons.forEach(button => {
+            button.disabled = true;
+            button.classList.add("noHover")
+        });
         return true;
     }
     return false;
 }
 
 
+/**
+* Set the text that shows who win the game after 5 rounds
+*/
 function winnerDecision() {
 
     let playerScore = parseInt((document.querySelector("#player-score")).textContent);
@@ -67,10 +88,14 @@ function winnerDecision() {
     else {
         roundResult.textContent = "Computer wins the game!"
     }
-
-
 }
+//#endregion
 
+// #region Click functions 
+
+/**
+* player decision on each round click handler
+*/
 function playerClick(e){
     playRound(e.target.className, computerPlay());
     if(isGameOver()) {
@@ -78,7 +103,11 @@ function playerClick(e){
     }
 }
 
+/**
+* handle the complete reset when pushing the reset button
+*/
 function resetClick(){
+
     const playerButtons = document.querySelectorAll(".btn");
     let playerScore = document.querySelector("#player-score");
     let computerScore = document.querySelector("#computer-score");
@@ -87,12 +116,17 @@ function resetClick(){
     playerScore.textContent = '0';
     computerScore.textContent = '0';
     roundResult.style.opacity = "0%";
-    playerButtons.forEach(button => button.classList.remove("noHover"));
-    playerButtons.forEach(button => button.disabled = false);
+    playerButtons.forEach(button => {
+         button.classList.remove("noHover");
+         button.disabled = false;
+        });
+
     MATCHDURATION = 0;
 }
 
+//#endregion
 
+/* add the listener to the buttons */
 const playerButtons = document.querySelectorAll(".btn");
 playerButtons.forEach(button => button.addEventListener('click', playerClick));
 
